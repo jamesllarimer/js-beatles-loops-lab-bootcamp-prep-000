3f32f0fa6451a0fe020a12d36d3096947b9e6b6e
James Lefty Larimer <lefty1044@gmail.com>
2018-07-05 16:53:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function theBeatlesPlay(musicians, instruments){
   }
   return stringArr;
 }
+
+function johnLennonFacts(musicians, instruments){
+  let stringArr = [];
+  for (var i = 0; i < musicians.length; i++){
+    stringArr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return stringArr;
+}
