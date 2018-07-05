6e7cad0d58e356e620cbfa971f57bf958369159a
James Lefty Larimer <lefty1044@gmail.com>
2018-07-05 16:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   let stringArr = [];
   for (var i = 0; i < musicians.length; i++){
-    stringArr.push(`${musicians[i]} plays the ${instruments[i]}`)
+    stringArr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return stringArr;
 }
